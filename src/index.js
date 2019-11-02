e84073245f16e302c109d8ba1f0e4e048e0beeca
Iran Reyes <iran.reyes@jam3.com>
2019-11-02 02:28:43 -0400

    chore: move lighthouse running to plugin hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 'use strict';
 
 const path = require('path');
-const requireIndex = require('requireindex');
 
-const plugin = {
-  rules: requireIndex(path.join(__dirname, '/rules'))
+const requireIndex = require('requireindex');
+const chromeLauncher = require('chrome-launcher');
+const lighthouse = require('lighthouse');
+const isURL = require('is-url');
+const Adviser = require('adviser');
+
+class LighthousePlugin extends Adviser.Plugin {
+  constructor(settings) {
+    super(settings);
+
+    this.rules = requireIndex(path.join(__dirname, '/rules'));
+
+    if (!settings.url || !isURL(settings.url)) {
+      throw new Error(`No valid url provided.`);
+    }
+
+    this.url = settings.url;
+  }
+
+  async preRun(context) {
+    const opts = {
+      chromeFlags: ['--show-paint-rects']
+    };
+
+    const chrome = await chromeLauncher.launch({ chromeFlags: opts.chromeFlags });
+    opts.port = chrome.port;
+    const results = await lighthouse(this.url, opts);
+    await chrome.kill();
+
+    if (!results.lhr) {
+      throw new Error('No results returned.');
+    }
+
+    context.addShareableData(results.lhr);
+  }
+}
+
+LighthousePlugin.meta = {
+  description: 'Lighthouse wrapper',
+  recommended: true
 };
 
-module.exports = plugin;
+module.exports = LighthousePlugin;
